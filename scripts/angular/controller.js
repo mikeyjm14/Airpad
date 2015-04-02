73a110ec151e6ecd8df527b5a0040cf175d63f69
Michael Morguarge <mikeyjm14@gmail.com>
2015-04-02 19:33:13 -0400

    An update to the front end look and feel.

--- scripts/angular/controller.js
+++ scripts/angular/controller.js
@@ -1,3 +1,12 @@
-AirPadApp.controller('', [
-	
+var notepad = function($scope, $state, $stateParams) {
+    $scope.currUser = "John Doe";
+
+    $scope.note = "John Doe";
+
+    $scope.notes = "John Doe";
+};
+
+AirPadApp.controller('NotePad', [
+	'$scope',
+    notepad
 ]);
