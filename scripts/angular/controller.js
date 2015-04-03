5522c41ce60cf6529725c105ad8a1b4f73331a57
Michael Morguarge <mikeyjm14@gmail.com>
2015-04-03 04:07:26 -0400

    Created an example version of the notes view page.

--- scripts/angular/controller.js
+++ scripts/angular/controller.js
@@ -4,9 +4,84 @@ var notepad = function($scope, $state, $stateParams) {
     $scope.note = "John Doe";
 
     $scope.notes = "John Doe";
+
+    $scope.GoToNote = function() {
+        $state.go("viewnote", {noteID: "abcd"});
+    };
+
+    $scope.listOfNoNotes = {
+        notes: []
+    };
+
+    $scope.listOfNotes = {
+        notes: [
+            {
+                title: "Note 1",
+                content: "Content",
+                creator: $scope.currUser,
+                creationDate: "Some date",
+                recentEditDate: "Some date",
+                categories: ["none yet", "none added yet", "none to add yet"],
+                id: "Some id"
+            },
+            {
+                title: "Note 2",
+                content: "Content",
+                creator: $scope.currUser,
+                creationDate: "Some date",
+                recentEditDate: "Some date",
+                categories: ["none yet"],
+                id: "Some id"
+            },
+            {
+                title: "Note 3",
+                content: "Content",
+                creator: $scope.currUser,
+                creationDate: "Some date",
+                recentEditDate: "Some date",
+                categories: ["none yet"],
+                id: "Some id"
+            },
+            {
+                title: "Note 4",
+                content: "Content",
+                creator: $scope.currUser,
+                creationDate: "Some date",
+                recentEditDate: "Some date",
+                categories: ["none yet"],
+                id: "Some id"
+            }
+        ]
+    };
+
+    $scope.optionsTitle = {
+        height: 230,
+        maxHeight: 230,
+        width: 500,
+        styleWithSpan: true,
+        focus: true,
+        airMode: false,
+        toolbar: [
+            ['edit',['undo','redo']],
+            ['headline', ['style']],
+            ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript', 'strikethrough', 'clear']],
+            ['fontface', ['fontname']],
+            ['textsize', ['fontsize']],
+            ['fontclr', ['color']],
+            ['alignment', ['ul', 'ol', 'paragraph', 'lineheight']],
+            ['height', ['height']],
+            ['table', ['table']],
+            ['insert', ['link','picture','video','hr']],
+            ['view', ['fullscreen', 'codeview']],
+            ['help', ['help']]
+        ]
+    };
+
 };
 
 AirPadApp.controller('NotePad', [
 	'$scope',
+    '$state',
+    '$stateParams',
     notepad
 ]);
